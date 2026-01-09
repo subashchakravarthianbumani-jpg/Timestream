95e377bd729bbfd51f1550789ba7dfe8d94db978
subashpixous <subashchakravarthianbumani@gmail.com>
2026-01-09 15:22:15 +0530

    action

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
   //apiUrl: '__API_URL__',
+  apiUrl: '/api'
   // apiUrl: 'http://3.110.188.52/api',
-  apiUrl: 'https://localhost:7074/api',
-  API_BASE_URL : 'http://3.108.9.185:5050',
+  // apiUrl: 'https://localhost:7074/api',
+  // API_BASE_URL : 'http://3.108.9.185:5050',
   //apiUrl: 'http://time.vsapps.in/api/',
   // apiUrl: 'https://tahdcotime.pixous.info/api/',
   // apiUrl: 'https://time.tahdco.com/api/',
